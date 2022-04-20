77925949270f7f05170b1dedebdda823915e74e8
by1459 <by1459@cs.washington.edu>
2022-04-19 20:36:31 -0700

    Got every team's stats from the 2021-2022 season

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -6,14 +6,26 @@ from selenium import webdriver
 
 chromedriver_path = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(chromedriver_path)
-url = "https://www.nba.com/stats/teams/traditional/?sort=W_PCT&dir=-1&Season=2019-20&SeasonType=Regular%20Season"
+url = "https://www.nba.com/stats/teams/traditional/?sort=W_PCT&dir=-1&Season=2021-22&SeasonType=Regular%20Season"
+stats = []
+
 driver.get(url)
 time.sleep(1)
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'html.parser')
-print(soup)
+#print(soup)
 teams = soup.find_all(
     "td"
 )
 for team in teams:
-    print(team)
+    print(team.text.strip())
+    """
+    stat = team.next
+    if stat.name == "a":
+        stats.append(stat.name)
+    else:
+        stats.append(team.next)
+    """
+#for stat in stats:
+ #   print(stat)
+
