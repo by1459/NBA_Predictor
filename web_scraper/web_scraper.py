0eb52e9de4068e61ded935b81862f4d0a204b0a5
by1459 <by1459@cs.washington.edu>
2022-04-24 12:48:09 -0700

    Added a few more get methods in the WebsiteScraper class

--- web_scraper/web_scraper.py
+++ web_scraper/web_scraper.py
@@ -5,43 +5,60 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 
 
-class Website_Scraper:
-    def __init__(self, given_url):
+class WebsiteScraper:
+    def __init__(self, given_url, champs_url, year, time_span):
         # Loading the website
         chromedriver_path = "/usr/local/bin/chromedriver"
-        driver = webdriver.Chrome(chromedriver_path)
-        url = given_url
-        driver.get(url)
+        driver1 = webdriver.Chrome(chromedriver_path)
+        driver2 = webdriver.Chrome(chromedriver_path)
+        url1 = given_url
+        driver1.get(url1)
+        url2 = champs_url
+        driver2.get(url2)
 
         # Getting the HTML data from the website
         time.sleep(1)
-        page_source = driver.page_source
-        self.soup = BeautifulSoup(page_source, 'html.parser')
-        self.years = -1
+        page_source1 = driver1.page_source
+        page_source2 = driver2.page_source
+        self.soup = BeautifulSoup(page_source1, 'html.parser')
+        self.soup_champs = BeautifulSoup(page_source2, 'html.parser')
+        self.year = year
+        self.time_span = time_span
 
-    def get_season_stats(self, table_rows, table_columns):
-        # Finding the table columns
-        header = ["RANKING", "TEAM"]
-        unformatted_header = self.soup.find_all(
-            "th", attrs={"data-dir": "-1"}
-        )
-        for header_title in unformatted_header:
-            header.append(header_title.text.strip())
-
+    def get_season_stats(self, table_rows, table_columns, header):
         # Getting the stats itself from an individual season
         teams = self.soup.find_all(
             "td"
         )
         read_stats = [] * table_rows
-        read_stats.append(header)
         for i in range(table_rows):
             row = []
             for j in range(len(header)):
                 row.append(teams[i * table_columns + j].text.strip())
             read_stats.append(row)
+        return read_stats
+
+    def get_headers(self, preset_headers):
+        # Finding the table columns
+        header = preset_headers
+        unformatted_header = self.soup.find_all(
+            "th", attrs={"data-dir": "-1"}
+        )
+        for header_title in unformatted_header:
+            header.append(header_title.text.strip())
+        return header
+
+    def get_champs(self):
+        unformatted_champs = self.soup_champs.find_all(
+            "td", attrs={"data-stat": "champion"}
+        )
+        unformatted_champs.pop(0)
+        champs = []
+        for i in range(self.time_span):
+            champs.append(unformatted_champs[i].text.strip())
+        return champs
 
     def get_years(self, num_years):
-        self.years = num_years
         years = [] * num_years
         unformatted_years = self.soup.find_all(
             "option"
@@ -60,12 +77,16 @@ class Website_Scraper:
 
 
 def main():
-    website_scraper = Website_Scraper(
-        "https://www.nba.com/stats/teams/traditional/?sort=W_PCT&dir=-1&Season=2021-22&SeasonType=Regular%20Season")
+    TIME_SPAN = 26
+    website_scraper = WebsiteScraper(
+        "https://www.nba.com/stats/teams/traditional/?sort=W_PCT&dir=-1&Season=2021-22&SeasonType=Regular%20Season",
+    "https://www.basketball-reference.com/playoffs/", 2022, TIME_SPAN)
+    website_scraper.get_champs()
     years = website_scraper.get_years(26)
-    for year in years:
-
-
+    dataset = [website_scraper.get_headers(["WINNER", "RANKING", "TEAM"])]
+    print(dataset)
+    #for year in years:
+    #    print(year)
 
 
 if __name__ == "__main__":
